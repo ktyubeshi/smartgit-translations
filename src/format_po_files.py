081b5c4c99429b7521154a49afabfd8b5c448876
ktyubeshi <ktyubeshi@users.noreply.github.com>
2025-07-03 02:58:28 +0900

    feat(scripts): Added a feature to specify the files to be processed as arguments to format_po_files.py

--- src/format_po_files.py
+++ src/format_po_files.py
@@ -1,10 +1,28 @@
+import argparse
+import os
 import sgpo
 from path_finder import PoPathFinder
 
 
 def main():
-    finder = PoPathFinder()
-    po_files = finder.get_po_files(translation_file_only=True)
+    parser = argparse.ArgumentParser(description="Format SmartGit PO files.")
+    parser.add_argument(
+        'file',
+        nargs='?',
+        default=None,
+        help='The PO file to format. If not provided, all PO files will be formatted.'
+    )
+    args = parser.parse_args()
+
+    po_files = []
+    if args.file:
+        if not os.path.exists(args.file):
+            print(f"Error: File not found at {args.file}")
+            exit(-1)
+        po_files.append(args.file)
+    else:
+        finder = PoPathFinder()
+        po_files = finder.get_po_files(translation_file_only=True)
 
     for po_file in po_files:
         try:
@@ -16,6 +34,7 @@ def main():
 
         po.format()
         po.save(po_file)
+
 
 if __name__ == "__main__":
     main()
